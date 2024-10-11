3795487a55b33a5eae75e2cd92a1c52b60f6582f
Stuart Cragg <stuart.cragg@clouddirect.net>
2024-10-11 17:29:10 +0200

    renamed workflow

---
+++ index.js
@@ -0,0 +1,9 @@
+const pulumi = require("@pulumi/pulumi");
+const azure = require("@pulumi/azure-native");
+
+const resourceGroup = new azure.resources.ResourceGroup("myResourceGroup", {
+    resourceGroupName: "rg-pulumi-language-bot-uks-001",
+    location: "uksouth",
+});
+
+exports.resourceGroupName = resourceGroup.name;
