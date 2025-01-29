3a9ccee29878bb3d3dd8a9c828db523297cd2bb6
Stuart Cragg <stuart.cragg@capgemini.com>
2025-01-29 13:55:09 +0100

    changed resource group name

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const pulumi = require("@pulumi/pulumi");
 const azure = require("@pulumi/azure-native");
 
 const resourceGroup = new azure.resources.ResourceGroup("myResourceGroup", {
-    resourceGroupName: "rg-pulumi-language-bot-uks-001",
+    resourceGroupName: "rg-pulumi-language-trainer-uks-001",
     location: "uksouth",
 });
 
